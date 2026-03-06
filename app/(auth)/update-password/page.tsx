c9c0470b57cf1a464a4561fd2719dfab55b03ba1
Tim Chaffee <github@handstand.biz>
2026-03-05 21:41:09 -0500

    Added suspense from build feedback

--- app/(auth)/update-password/page.tsx
+++ app/(auth)/update-password/page.tsx
@@ -1,7 +1,10 @@
+import { Suspense } from 'react'
 import { UpdatePasswordForm } from "./update-password-form";
 
 export default function UpdatePasswordPage() {
     return (
-        <UpdatePasswordForm />
+        <Suspense fallback={<>...</>}>
+            <UpdatePasswordForm />
+        </Suspense>
     );
 }
